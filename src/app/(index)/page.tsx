64f7b902399a57e5e4ee6cb49ef82ccb9066bbcd
Alex Luowan <aaluowan@gmail.com>
2023-08-29 21:29:26 -0700

    added thumbnails and footer component

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <div className="hidden col-start-8 col-span-6 row-start-1 lg:block">
         <nav className="font-untitledsans text-base mt-4">
           <ul>
-            <li><a href="about.html" className="underline">About</a></li>
+            <li><a href="" className="underline">About</a></li>
             <li><a
                 href="https://www.figma.com/file/TWVrzXVl7Eg3VE8F2BPWRG/Portfolio-webpage?type=design&node-id=240%3A46&mode=design&t=sJyuJqh07gzBG8pO-1"
                 className="underline mt-2">Resume</a></li>
@@ -39,11 +39,30 @@ export default function Home() {
         </div>
 
         <div className="col-start-1 col-span-full lg:col-span-7 lg:row-start-3 mt-5 lg:mt-8">
-            <a href="">
-
+            <a href="#">
+                <Image src="/img/givemeyesterday/Givemeyesterdaythumbnail.png"
+                       alt="a laptop displaying a microsite called give me yesterday"  width={2500} height={1620}/>
+                <div className="font-untitledsans text-base mt-4">
+                    <h4 className="text-customGray">Interaction design, Art Direction</h4>
+                    <h4>Give me yesterday</h4>
+                </div>
             </a>
         </div>
 
+        <div className="col-start-1 lg:col-start-8 col-span-full lg:col-span-5 lg:row-start-3 mt-14 lg:mt-8">
+            <a href="#">
+                <Image src="/img/drunkelephant/drunkelephantthumbnail.png"
+                       alt="a laptop displaying a intervention of a redesigned version of drunk elephant" width={2500} height={1512}/>
+                <div className="font-untitledsans text-base mt-4">
+                    <h4 className="text-customGray">UX/UI Design</h4>
+                    <h4>Drunk Elephant</h4>
+                </div>
+            </a>
+        </div>
+        
     </section>
+
+
+
   )
 }
