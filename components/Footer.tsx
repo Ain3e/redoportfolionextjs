abc543cbf6fe8051e33b050c5c0aa3ee8a04f26a
Alex Luowan <aaluowan@gmail.com>
2023-08-30 01:08:15 -0700

    Changed footer component code

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,12 @@
 export default function Footer() {
     return(
-        <footer className="mx-6 grid grid-cols-4 gap-4 mt-20 mb-10 md:grid-cols-6 lg:grid-cols-12">
-                <div className="col-start-1 col-span-4">
-                    <p className="font-untitledsans text-customGray">©2023 Design and Coded by Alex Luowan, All Rights Reserved.
-                    </p>
-                </div>
-        </footer>
+        <section className="mx-6 grid grid-cols-4 gap-4 mt-20 mb-10 md:grid-cols-6 lg:grid-cols-12 ">
+            <div className="col-start-1 col-span-4">
+                <p className="font-untitledsans text-customGray">©2023 Design and Coded by Alex Luowan, All Rights
+                    Reserved.
+                </p>
+            </div>
+        </section>
     );
 }
 
