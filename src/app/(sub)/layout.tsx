8fd998294ae5809915a5b15e409a53d4c20d5cd1
Alex Luowan <aaluowan@gmail.com>
2023-08-30 01:29:51 -0700

    Moved components back to original location

--- src/app/(sub)/layout.tsx
+++ src/app/(sub)/layout.tsx
@@ -1,7 +1,7 @@
 import '../globals.css'
 import type { Metadata } from 'next'
 import localFont from'next/font/local'
-import Footer from '@/app/components/Footer'
+import Footer from '../../../components/Footer'
 
 const eiko = localFont({
     src: [
