ebc7a0d04f879e346228e522f743e5ea28331415
Alex Luowan <aaluowan@gmail.com>
2023-08-30 01:42:28 -0700

    Updated margin to div for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return(
-        <footer className="mx-6 grid grid-cols-4 gap-4 mt-20 mb-10 md:grid-cols-6 lg:grid-cols-12">
-                <div className="col-start-1 col-span-4">
+        <footer className="mx-6 grid grid-cols-4 gap-4 md:grid-cols-6 lg:grid-cols-12">
+                <div className="col-start-1 col-span-4 mt-20 mb-10">
                     <p className="font-untitledsans text-customGray">©2023 Design and Coded by Alex Luowan, All Rights Reserved.
                     </p>
                 </div>
