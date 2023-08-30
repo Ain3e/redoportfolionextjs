2a7327fdd5d0b961775c717adc299dc4538ba7b9
Alex Luowan <aaluowan@gmail.com>
2023-08-30 01:48:04 -0700

    Revert "moved components back to base folder"
    This reverts commit 2cf9437788ac6e200b23d996d1507f067ec2782e.

--- src/app/(index)/layout.tsx
+++ src/app/(index)/layout.tsx
@@ -1,7 +1,7 @@
 import '../globals.css'
 import type { Metadata } from 'next'
 import localFont from'next/font/local'
-import Footer from '../../../components/Footer'
+import Footer from '@/components/Footer'
 
 const eiko = localFont({
   src: [
